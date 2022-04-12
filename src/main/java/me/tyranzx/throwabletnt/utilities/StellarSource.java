a90674b539ad5e18005f25d410445bdfe6517782
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2022-04-12 12:30:01 -0700

    Create StellarSource.java

---
+++ src/main/java/me/tyranzx/throwabletnt/utilities/StellarSource.java
@@ -0,0 +1,15 @@
+package me.tyranzx.throwabletnt.utilities;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.FallingBlock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StellarSource {
+
+    public static List<FallingBlock> ttnt = new ArrayList<>();
+    public static String c(String msg){
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+}
